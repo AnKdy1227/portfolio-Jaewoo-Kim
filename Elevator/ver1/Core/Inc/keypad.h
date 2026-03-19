19f1d4fdfb3218e3432e121263df7fde1d4bae55
andyK <asertoved@naver.com>
2026-03-20 01:37:24 +0900

    Add file

---
+++ Elevator/ver1/Core/Inc/keypad.h
@@ -0,0 +1,18 @@
+/*
+ * keypad.c
+ *
+ *  Created on: Sep 23, 2025
+ *      Author: andykim02
+ */
+
+
+#ifndef INC_KEYPAD_H_
+#define INC_KEYPAD_H_
+
+// main.h를 인클루드하여 HAL 드라이버 정의를 가져옵니다.
+#include "main.h"
+
+// 함수 선언
+char read_keypad(void);
+
+#endif /* INC_KEYPAD_H_ */
